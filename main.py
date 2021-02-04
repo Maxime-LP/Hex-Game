e08600f479cb1e36275833730d8745ab4a374439
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2021-02-04 20:37:22 +0100

    playable_hex

---
+++ main.py
@@ -0,0 +1,68 @@
+import pygame
+import numpy as np
+from misc import get_polygon,tiles_centers
+
+pygame.init()
+
+screen = pygame.display.set_mode((1300,900))
+background = pygame.image.load("Hex_board.png")
+
+#   0__________________x=1300
+#   |
+#   | (x0,y0)
+#   |  
+#   |       screen
+#   |
+#   y=900
+
+
+#appliquer l'image du plateau de jeu
+screen.blit(background,(0,0))
+
+#plateau lisible par nos algorithmes
+plateau=[['.' for i in range(11)] for j in range(11)]
+plateau=np.array(plateau)
+
+
+#####Initialisation
+#Liste des hexagones posés
+played_tiles=[]
+#Le joueur bleu commence
+color="blue"
+pygame.display.set_caption("Homemade Hex Game")
+last_played_tile=None
+running=True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            running = False
+            break
+        
+        #Lorsque le joueur appuie sur ECHAP
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                break
+
+        #Au clic gauche de la souris
+        elif event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed(num_buttons=3)==(True,False,False):
+            #On récupère la position de la souris sous la forme pos=(x,y)
+            pos=pygame.mouse.get_pos()
+
+            if background.get_at(pos)==(223, 223, 223, 255):
+                points,center=get_polygon(pos)
+
+                if center not in played_tiles:
+                    #On dessine l'hexagone au lieu sélectionné et on change de joueur
+                    pygame.draw.polygon(screen,color,points)
+                    last_played_tile=center
+                    played_tiles.append(center)
+                    index=tiles_centers.index(center)
+                    i,j=index//11,index%11 #on pourra verifier que si q et r sont tq index=11q+r alors on a bien i,j=q,r
+                    plateau[i,j]=color
+                    print(plateau)
+                    if color=="red": color="blue"
+                    else: color="red"
+    
+    pygame.display.flip()
---
+++ misc.py
@@ -0,0 +1,41 @@
+import scipy.linalg as lg
+
+#Centre de notre repère "fait-maison"
+(x0,y0)=(106,128)
+
+
+#On se donne la liste des centres des hexagones
+tiles_centers=[]
+y0-=20 #initial shift
+x0-=67
+for i in range(1,12):
+    y0=y0+57.7
+    x0+=33.6
+    for j in range(1,12):
+        point=(x0+j*66.7,y0)
+        tiles_centers.append(point)
+
+def get_polygon(point):
+    """
+    Retourne la liste des points déterminant le polygone contenant le point en argument
+    """
+    l=64
+    h=74.3
+
+    min_point=tiles_centers[0]
+    k=0
+    while True:
+        try:
+            p=tiles_centers[k]
+            diff1=(p[0]-point[0],p[1]-point[1])
+            diff2=(min_point[0]-point[0],min_point[1]-point[1])
+            if lg.norm(diff1)<lg.norm(diff2):
+                min_point=tiles_centers[k]
+        except IndexError:
+            break
+        k+=1
+    
+    x,y=min_point
+    points=[(x+l/2,y-h/4),(x+l/2,y+h/4),(x,y+h/2),(x-l/2,y+h/4),(x-l/2,y-h/4),(x,y-h/2)]
+    return points,min_point
+
