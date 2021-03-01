0e347a70436b5a0ba8e50cb08e27415822f1a32c
amait41 <adrien.maitammar@gmail.com>
2021-03-01 20:36:11 +0100

    fun best action

--- AI/mean/best_action_average.py
+++ AI/mean/best_action_average.py
@@ -14,30 +14,26 @@ def best_action_average(board, n, color):
     (RED, BLUE) = (1, 2) if color == 1 else (2,1)
     player1 = AI_player(RED)
     player2 = AI_player(BLUE)
-    tgame = Game(board, player1, player2)
 
-    for action in tgame.board.actions:
-        #tgame = copy(game) #Game(tboard, player1, player2)
-        i, j = tgame.board.action_to_coord(action)
-        tgame.players[0].plays(tgame.board, i, j)
-        print(board)
-        print(tgame.board)
-        print(board is tgame.board)
-        input('pause')
-        '''
+    for action in board.actions:
+        tboard = deepcopy(board)
+        game = Game(tboard, player1, player2)
+        i, j = game.board.action_to_coord(action)
+        game.players[0].plays(game.board, i, j)
+
         games_won = 0
 
         for _ in range(n):
-            tgame = game
+            tgame = deepcopy(game)
             winner = tgame.run()
             games_won += 1 if winner == color else 0
 
         res[action] = games_won
-        '''
-        #game.board.board[i][j] = 0
+
+    print(res)
 
     best_action = max(res, key = res.get)
-    
+    print(best_action)
     return best_action
-
+    
 #####################################################
