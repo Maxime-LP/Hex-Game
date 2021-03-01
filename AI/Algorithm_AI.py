1015dd38556d41fc15a2d73da2e3748a1701adf5
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2021-03-02 00:36:59 +0100

    Update Algorithm_AI.py

--- AI/Algorithm_AI.py
+++ AI/Algorithm_AI.py
@@ -10,7 +10,7 @@ def run_mean(board, color):
     Jouer N parties sur chaque case libre et voir quelle case a le meilleur score
     Y-a-t-il une meilleure valeur de N ? On peut augmenter la valeur de N à chaque coup car moins de cases à tester
     """
-    n = 100
+    n = 10
     action = best_action(board, n, color)
     return board.action_to_coord(action)
 
