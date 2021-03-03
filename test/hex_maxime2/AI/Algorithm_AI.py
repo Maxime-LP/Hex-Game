7c5b86cb5f139f6d011811946f4a6dd45ebd4590
amait41 <adrien.maitammar@gmail.com>
2021-03-03 12:00:38 +0100

    test rapidité check_win connnexe

---
+++ test/hex_maxime2/AI/Algorithm_AI.py
@@ -0,0 +1,21 @@
+from random import choice
+from AI.mean.best_action import best_action
+
+def run_random(board, color):
+    i, j = board.action_to_coord(choice(board.actions))
+    return i, j
+
+def run_mean(board, color):
+    """
+    Jouer N parties sur chaque case libre et voir quelle case a le meilleur score
+    Y-a-t-il une meilleure valeur de N ? On peut augmenter la valeur de N à chaque coup car moins de cases à tester
+    """
+    n = int(5000 / board.size**2)
+    action = best_action(board, n, color)
+    return board.action_to_coord(action)
+
+def run_ucb1(board):
+    return
+
+def run_mcts(board):
+    return
---
+++ test/hex_maxime2/AI/mean/coord_aux.py
@@ -0,0 +1,27 @@
+import re
+import string
+
+def get_x_index(letter):
+    """ The X index is the "line" part of the board. Because coordinate is a letter to represent it,
+    we need his position in alphabet.
+    To begin at the 1 index, we add 1 to list index. """
+    return list(string.ascii_uppercase).index(letter.upper()) + 1
+
+
+def get_coord(position):
+    """ Using a regex, this function checks if the position is correct """
+    regex = re.match("^([a-zA-Z]{1})([0-9]{1,2})$", position)
+
+    return regex.groups() if regex is not None else regex
+
+def is_outside(board, line, column):
+    """
+    Check if the stone can be put inside the board
+    """
+    length = len(board)
+    return length <= line or length <= column
+
+
+def is_already_taken(board, line, column):
+    """ Check if the place is already taken """
+    return board[column][line] == 1
