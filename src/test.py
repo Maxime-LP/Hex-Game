be3ada44ed830da4f34d8fe40e7b028e8a126056
amait41 <adrien.maitammar@gmail.com>
2021-04-14 23:40:11 +0200

    add multiprocessing

--- src/test.py
+++ src/test.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
 import sys
+import os
 from time import time
+from multiprocessing import Process
 import numpy as np
 import matplotlib.pyplot as plt
 from Board import Board
@@ -7,9 +10,7 @@ from Game import Game
 from Player import AI
 
 
-
-def test(testType, player1_type, player2_type, board_size):
-
+def test(testType, player1_type, player2_type, board_size, n=None):
     RED, BLUE = 1, 2
     ai_algorithms = ['random', 'mc', 'mc_ucb1', 'mcts']
     # init red player
@@ -26,31 +27,29 @@ def test(testType, player1_type, player2_type, board_size):
     tests = {'test1': test1,
              'test2': test2}
     make_test = tests[sys.argv[5]]
-    make_test(player1, player2, board_size, 2000)
+    make_test(player1, player2, board_size, 5)
 
 
 def test1(player1, player2, board_size, n):
-    """find the """
-    print('\nSimulations in progress...')
+    print('Simulations in progress...')
     time0 = time()
     RED, BLUE = 1, 2
     C = np.linspace(0.05,1,5)
     res = []
-
     if player2.algorithm.__name__ != 'mcts':
         raise Exception("Player 2 type must be mcts for test1.")
 
     for explorationConstant in C:
-        print('explor_cst:',explorationConstant)
+        print('\nexplor_cst:', explorationConstant)
         player2.explorationConstant = explorationConstant
         mcts_winrate = 0
         for i in range(n):
-            if i%2 == 0: print(f'{i} - ', end=' ')
+            print(i)
             board = Board(board_size)
             game = Game(board, player1, player2)
             mcts_winrate += game.runNoDisplay()
-
         res.append(mcts_winrate / n)
+
     print(f'Execution : {time()-time0}s')
 
     plt.plot(C, res, marker='o')
@@ -59,25 +58,54 @@ def test1(player1, player2, board_size, n):
     plt.title(f"UCT's winrate on {n} games vs {player1.algorithm.__name__}", 
           fontdict={'color' : 'darkblue',
                     'size': 14})
-    #plt.show()
     plt.savefig(f"simulations/{time()}.png")
     
 
 def test2(player1, player2, board_size, n):
-    print('\nSimulations in progress...')
-    if player2.algorithm.__name__=='random':
-        n = 100000
-    else:
-        n = 10
+    #print('Simulations in progress...')
     time0 = time()
     w = 0
     for i in range(n):
         board = Board(board_size)
         game = Game(board, player1, player2)
-        w += game.runNoDisplay()    
-    print(f'#games = {n}')
-    print(f'Win rate Blue: {w/n}')
-    t = time()-time0
-    print(f'Time for {n} games: {round(t,3)}s')
-    print(f'{round(n/t,3)} games/s')
-    print(f'{round(n*60/t,3)} games/min')
+        w += game.runNoDisplay() 
+    if __name__!='__main__':
+        print(f'Win rate Blue: {w/n}')
+        t = time()-time0
+        print(f'{round(n/t,3)} games/s - {round(n*60/t,3)} games/min')
+        print(f'Time for {n} games: {round(t,3)}s')
+    
+
+if __name__ == "__main__":
+    player1_type = sys.argv[1]
+    player2_type = sys.argv[2]
+    board_size = sys.argv[3]
+    testType = sys.argv[5]
+    n = 10
+    
+    processes = []
+    num_processes = 1 #os.cpu_count() to get num CPU
+    d, r = n//num_processes, n%num_processes
+    nb_games = [d] * (num_processes-1) + [d+r]
+
+    for i in range(num_processes):
+        process = Process(target=test,
+                          args=(testType,
+                                player1_type,
+                                player2_type,
+                                board_size,
+                                nb_games[i]))
+        processes.append(process)
+
+    print('Simulations in progress...')
+    print(f'#process: {len(processes)}')
+
+    time0 = time()
+
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    print(f'Time for {n} games: {round(time()-time0,2)}s')
