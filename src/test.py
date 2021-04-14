0e2920c090e319ceea0ac03d5ed6ab65a6add4b6
amait41 <adrien.maitammar@gmail.com>
2021-04-14 14:25:49 +0200

    add time

--- src/test.py
+++ src/test.py
@@ -26,10 +26,10 @@ def test(testType, player1_type, player2_type, board_size):
     tests = {'test1': test1,
              'test2': test2}
     make_test = tests[sys.argv[5]]
-    make_test(player1, player2, board_size, 1)
+    make_test(player1, player2, board_size, 10)
 
 
-def test1(player1, player2, board_size, n=10000):
+def test1(player1, player2, board_size, n=10):
     """find the """
     print('Simulations in progress...')
     time0 = time()
