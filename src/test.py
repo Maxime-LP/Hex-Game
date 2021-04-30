c1d7c6a7bf982ce0d17cba17bb2b60ab52d7af18
amait41 <adrien.maitammar@gmail.com>
2021-04-30 08:57:40 +0200

    prepare simu

--- src/test.py
+++ src/test.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     start_time = time()
     
     n = int(sys.argv[4])
-    cst_list = np.round(list(np.linspace(0,4,21)),3)
+    cst_list = [0.6] #np.round(list(np.linspace(0,4,21)),3)
     result = []
     
     for c in tqdm(cst_list):
