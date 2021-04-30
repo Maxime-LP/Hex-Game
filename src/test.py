54fd9ef9f0df42e87bfc45d38ded7ae74a1748d4
amait41 <adrien.maitammar@gmail.com>
2021-04-30 08:39:41 +0200

    prepare simu

--- src/test.py
+++ src/test.py
@@ -47,6 +47,9 @@ def test(args):
 
     if c != None:
         player2.explorationConstant = c
+    print(player1.explorationConstant)
+    print(player2.explorationConstant)
+
 
     board = Board(board_size)
     game = Game(board, player1, player2)
@@ -60,7 +63,7 @@ if __name__ == "__main__":
     start_time = time()
     
     n = int(sys.argv[4])
-    cst_list = [0.3] #np.round(list(np.linspace(0,1,21)),3)
+    cst_list = np.round(list(np.linspace(0,4,21)),3)
     result = []
     
     for c in tqdm(cst_list):
